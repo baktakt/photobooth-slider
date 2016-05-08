5c62812e80ddb88dc7fd3616febbc9f246e47e62
Johan <baktakt@gmail.com>
2016-05-08 18:45:00 +0200

    Added missing stuff

---
+++ gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function(grunt) {
+  grunt.initConfig ({
+    sass: {
+      dist: {
+        files: {
+          'public/css/style.css' : 'src/scss/application.scss'
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      source: {
+        files: ['gruntfile.js','src/scss/**/*.scss','src/js/**/*.js'],
+        tasks: ['rebuild'],
+        options: {
+          livereload: false, // needed to run LiveReload
+        }
+      },
+      express: {
+        files:  ['views/**/*.ejs'],
+        tasks:  [ 'express:dev' ],
+        options: {
+          spawn: false // for grunt-contrib-watch v0.5.0+, "nospawn: true" for lower versions. Without this option specified express won't be reloaded
+        }
+      }
+    },
+    concat: {
+      dist: {
+        src: ['src/js/**/*.js'],
+        dest: 'public/scripts/main.js',
+      },
+    },
+    uglify: {
+      dist: {
+        files: {
+          'public/scripts/main.min.js': ['public/scripts/main.js'],
+        }
+      }
+    },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'public/css',
+          src: ['style.css', '!*.min.css'],
+          dest: 'public/css',
+          ext: '.min.css'
+        }]
+      }
+    },
+    express: {
+      dev: {
+        options: {
+          script: 'app.js'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-express-server');
+
+  grunt.registerTask('rebuild', ['sass', 'concat', 'uglify', 'cssmin']);
+  grunt.registerTask('dev', ['rebuild', 'express', 'watch']);
+  grunt.registerTask('default', ['sass', 'concat', 'uglify', 'cssmin']);
+
+
+};
